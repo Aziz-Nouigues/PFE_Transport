8adc5ac72ded0a9c01e83cd89d955e9afe5be7a0
aziz <aziznouigues77@gmail.com>
2026-02-26 13:47:37 +0100

    main structure

---
+++ odoo/custom_addons/transport_energy/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'transport_energy',
+    'author': 'Aziz Nouigues',
+    'category': '',
+    'version': '17.0.0.1.0',
+    'depends': ['base'],
+    'data': [
+
+    ],
+
+    'application': True,
+
+}
